5e73c6d8fb36351dadc404ffbfa0bf9f121e57e0
PiyushC1480 <122681250+PiyushC1480@users.noreply.github.com>
2023-04-08 18:30:19 +0530

    Add files via upload

--- Assign3-cop290-final-collab/main.py
+++ Assign3-cop290-final-collab/main.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, session, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 import json
+from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
 import math
 from datetime import datetime
+import os
+
 with open('config.json', 'r') as c:
     params = json.load(c)["params"]
 
@@ -16,6 +19,8 @@ if(local_server):
 app.debug = True
 db = SQLAlchemy(app)
 app.secret_key = "cop290 assignment 3"
+app.config['UPLOAD_FOLDER'] = params['upload_location']
+
 class Contacts(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
@@ -48,7 +53,7 @@ class User(db.Model):
     USERID = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), nullable=False)
     PostUploaded = db.Column(db.String(80), nullable=False)
-    ProfilePhoto = db.Column(db.String(80), nullable=False)
+    ProfilePhoto = db.Column(db.String(150), nullable=False)
     password = db.Column(db.String(80), nullable=False)
 
 class Recipes(db.Model):
@@ -69,11 +74,11 @@ class Recipes(db.Model):
 def home():
     lat_post = Posts.query.filter_by().all()
     recipe =  Recipes.query.filter_by().all()
-    return render_template("index.html" ,lat_post = lat_post , recipe = recipe)
+    return render_template("index.html" ,lat_post = lat_post , recipe = recipe,params = params)
 
 @app.route('/homechef/all_posts')
 def all_posts():
-    all_posts = Posts.query.filter_by().all()
+    all_posts = Posts.query.order_by(Posts.sno.desc()).all()
     last = math.ceil(len(all_posts)/int(params['no_of_posts']))
     #[0: params['no_of_posts']]
     #posts = posts[]
@@ -93,11 +98,11 @@ def all_posts():
     else:
         prev = "?page=" + str(page - 1)
         next = "?page=" + str(page + 1)
-    return render_template("all_posts.html", all_posts=all_posts, prev=prev, next=next)
+    return render_template("all_posts.html", all_posts=all_posts, prev=prev, next=next,params=params)
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", params=params)
 
 @app.route('/contact', methods = ['GET', 'POST'])
 def contact():
@@ -109,7 +114,7 @@ def contact():
         entry = Contacts(name=name,email = email, subject= subject,  message = message, date= datetime.now())
         db.session.add(entry)
         db.session.commit()
-    return render_template("contact.html")
+    return render_template("contact.html",params=params)
 
 @app.route('/homechef')
 def homechef():
@@ -120,6 +125,7 @@ def homechef():
 @app.route('/logout', methods = ['GET'])
 def logout():
     session['logged_in'] = False
+    params['path_of_img'] =""
     # redirecting to home page
     return redirect('/')
 
@@ -137,7 +143,8 @@ def login():
                 session['logged_in'] = True
                 session['email'] = user_list.email 
                 session['username'] = user_list.name
-                return redirect("/")
+                params['path_of_img'] = user_list.ProfilePhoto
+                return redirect("/" )
         else:
             flash('Username or Password Incorrect', "Danger")
             return render_template("login.html")
@@ -151,7 +158,7 @@ def pages_register():
         name = request.form.get('name')
         email = request.form.get('email')
         PostUploaded = ""
-        ProfilePhoto = ""
+        ProfilePhoto = "default.png"
         password = generate_password_hash(request.form.get('password'), method='sha256')
         entry = User(name=name,email = email,PostUploaded=PostUploaded,ProfilePhoto = ProfilePhoto, password = password)
         db.session.add(entry)
@@ -159,6 +166,7 @@ def pages_register():
         session['logged_in'] = True
         session['email'] = email 
         session['username'] = name
+        params['path_of_img']= ProfilePhoto 
         flash('You have successfully registered', 'success')
         return redirect("/")
 
@@ -170,25 +178,30 @@ def post_form():
         email = request.form.get('email')
         title = request.form.get('title')
         content = request.form.get('content')
-        images = request.form.get('images')
+        f = request.files['images']
+        if f.filename != '' :
+            f.save(os.path.join(app.root_path,"static/img/foodimg/", secure_filename(f.filename)))
+            images  = secure_filename(f.filename)
+        else:
+            flash('no image selected')
         slug = "food-post-"
-        entry = Posts(author=author,email = email, title= title,  content = content, date= datetime.now(),slug = slug,images = "images", likes=0)
+        entry = Posts(author=author,email = email, title= title,  content = content, date= datetime.now(),slug = slug,images = images , likes=0)
         db.session.add(entry)
         db.session.commit()
-    return render_template("post_form.html")
+    return render_template("post_form.html",params=params)
 
 @app.route('/homechef/all_posts/post_details/<string:post_slug>', methods = ['GET', 'POST'])
 def post_details(post_slug):
     post = Posts.query.filter_by(slug=post_slug).first()
     post_review = Post_review.query.filter_by(PostID = post.sno).all()
-    
+    usr = User.query.filter_by(name = post.author).first()
     if(request.method=='POST'):
         UserName = request.form.get('UserName')
         Review = request.form.get('Review')
         entry = Post_review(UserName = UserName, PostID = post.sno ,Review = Review, date= datetime.now())
         db.session.add(entry)
         db.session.commit()
-    return render_template('post-details.html', params=params, post=post, post_review = post_review)
+    return render_template('post-details.html', params=params, post=post, post_review = post_review,usr=usr)
 
 
 
@@ -200,19 +213,25 @@ def recipe_form():
         Title = request.form.get('Title')
         Ingredients = request.form.get('Ingredients')
         Instructions = request.form.get('Instructions')
-        images = request.form.get('images')
         slug = "recipe-post-"
         tagger = "postedrec"
-        entry = Recipes(RecipeAuthor=RecipeAuthor,Category = Category, Title= Title,  Ingredients = Ingredients,Instructions = Instructions, Date= datetime.now(), Slug = slug, Image = "images", Likes=0,Tagger = tagger)
+        f = request.files['images']
+        if f.filename != '' :
+            f.save(os.path.join(app.root_path,"static/img/foodimg/", secure_filename(f.filename)))
+            images  = secure_filename(f.filename)
+        else:
+            flash('no image selected')
+        entry = Recipes(RecipeAuthor=RecipeAuthor,Category = Category, Title= Title,  Ingredients = Ingredients,Instructions = Instructions, Date= datetime.now(), Slug = slug, Image = images, Likes=0,Tagger = tagger)
         db.session.add(entry)
         db.session.commit()
-    return render_template("recipe_form.html")
+    return render_template("recipe_form.html",params=params)
 
 @app.route('/recipes/recipe_details/<string:rec_slug>')
 def recipe_details(rec_slug):
     recipe = Recipes.query.filter_by(Slug=rec_slug).first()
+    auth = User.query.filter_by(name = recipe.RecipeAuthor).first()
     all_rec = Recipes.query.filter_by().all()
-    return render_template("recipe-details.html",recipe=recipe,all_rec = all_rec)
+    return render_template("recipe-details.html",params=params,recipe=recipe,all_rec = all_rec,auth=auth)
 
 
 @app.route('/recipes/search', methods = ['GET', 'POST'])
@@ -220,7 +239,7 @@ def search_recipe():
     search_term = request.form.get('searched')
     found_rec = Recipes.query.filter(Recipes.Title.like('%' + search_term + '%')).all()
     # found_rec = found_rec.order_by(Recipes.Title).all()
-    return render_template("recipes.html",all_rec = found_rec)
+    return render_template("recipes.html",all_rec = found_rec,params=params)
 
 
 @app.route('/recipes/categories/Breakfast', methods = ['GET', 'POST'])
@@ -228,41 +247,41 @@ def search_breakfast():
     search_term = "Breakfast"
     found_rec = Recipes.query.filter_by(Category = search_term).all()
     # found_rec = found_rec.order_by(Recipes.Title).all()
-    return render_template("recipes.html",all_rec = found_rec)
+    return render_template("recipes.html",all_rec = found_rec,params=params)
 
 @app.route('/recipes/categories/Lunch', methods = ['GET', 'POST'])
 def search_lunch():
     search_term = "Lunch"
     found_rec = Recipes.query.filter_by(Category = search_term).all()
     # found_rec = found_rec.order_by(Recipes.Title).all()
-    return render_template("recipes.html",all_rec = found_rec)
+    return render_template("recipes.html",all_rec = found_rec,params=params)
 @app.route('/recipes/categories/Dinner', methods = ['GET', 'POST'])
 def search_dinner():
     search_term = "Dinner"
     found_rec = Recipes.query.filter_by(Category = search_term).all()
     # found_rec = found_rec.order_by(Recipes.Title).all()
-    return render_template("recipes.html",all_rec = found_rec)
+    return render_template("recipes.html",all_rec = found_rec,params=params)
 
 @app.route('/recipes/categories/Appetizer', methods = ['GET', 'POST'])
 def search_appetizert():
     search_term = "Appetizer"
     found_rec = Recipes.query.filter_by(Category = search_term).all()
     # found_rec = found_rec.order_by(Recipes.Title).all()
-    return render_template("recipes.html",all_rec = found_rec)
+    return render_template("recipes.html",all_rec = found_rec,params=params)
 
 @app.route('/recipes/categories/Soups', methods = ['GET', 'POST'])
 def search_soups():
     search_term = "Soups"
     found_rec = Recipes.query.filter_by(Category = search_term).all()
     # found_rec = found_rec.order_by(Recipes.Title).all()
-    return render_template("recipes.html",all_rec = found_rec)
+    return render_template("recipes.html",all_rec = found_rec,params=params)
 
 @app.route('/recipes/categories/Desserts', methods = ['GET', 'POST'])
 def search_desserts():
     search_term = "Desserts"
     found_rec = Recipes.query.filter_by(Category = search_term).all()
     # found_rec = found_rec.order_by(Recipes.Title).all()
-    return render_template("recipes.html",all_rec = found_rec)
+    return render_template("recipes.html",all_rec = found_rec,params=params)
 
 @app.route('/recipes')
 def recipes():
@@ -286,11 +305,11 @@ def recipes():
     else:
         prev = "?page=" + str(page - 1)
         next = "?page=" + str(page + 1)
-    return render_template("recipes.html", all_rec=all_rec, prev=prev, next=next)
+    return render_template("recipes.html", all_rec=all_rec, prev=prev, next=next,params=params)
 
 @app.route('/shop')
 def shop():
-    return render_template("shop.html")
+    return render_template("shop.html", params=params)
     
 @app.route('/user_profile/<string:username>', methods = ['GET','POST'])
 def profile(username):
@@ -300,13 +319,18 @@ def profile(username):
     elif (request.method=='POST'):
         search_name.name= request.form.get('name')
         search_name.email = request.form.get('email')
+        f = request.files['ProfilePhoto']
+        if f.filename != '' :
+            f.save(os.path.join(app.root_path,"static/img/user_images", secure_filename(f.filename)))
+            search_name.ProfilePhoto  = secure_filename(f.filename)
         session['username'] = request.form.get('name')
         session['email'] = request.form.get('email')
+        params['path_of_img'] = search_name.ProfilePhoto 
         db.session.commit()
         n_pass= request.form.get('newpassword')
         nr_pass = request.form.get('renewpassword')
-        if (n_pass == nr_pass):
-            search_name.password = generate_password_hash(request.form.get('password'), method='sha256')
+        if (n_pass !="" and n_pass == nr_pass):
+            search_name.password = generate_password_hash(request.form.get('newpassword'), method='sha256')
             db.session.commit()
         else:
             flash("Password don't match")
